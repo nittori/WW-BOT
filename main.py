9a08b14298bb82a336a0ece90a3f5258a085dc24
nittori <76983266+nittori@users.noreply.github.com>
2022-08-29 12:45:58 +0900

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def tw_search(api,keyword):
         text: str = tweet.full_text.replace('\n','')
         
         #リクエスト過多を防ぐためのsleep
-        time.sleep(1)
+        time.sleep(10)
         #検索を完全一致に
         if "魔女兵器" in text:
             #取得したツイートがデータベースに登録済みならスルー
